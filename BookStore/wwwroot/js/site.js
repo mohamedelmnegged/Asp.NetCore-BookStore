7d576edc35b66a7bc4d9d4023f7758b5861f080d
mohamedelmnegged <mohamedaahmed92@gmail.com>
2022-01-05 16:37:36 +0200

    Add PopWindow and Ajax techniques

--- BookStore/wwwroot/js/site.js
+++ BookStore/wwwroot/js/site.js
@@ -1,1 +1,35 @@
 ﻿// Write your Javascript code.
+
+ShowPopUp = function (URL, Title) {
+    
+    $.ajax({
+        type: "GET",
+        url: URL,
+        success: function (result) {
+            $('#form-modal .modal-body').html(result);
+            $('#form-modal .modal-title').html(Title);
+            $('#form-modal').modal('show');
+
+        }
+    })
+}
+
+ClosePopUp = function () {
+    $('#form-modal').modal('hide');
+}
+
+searchResult = function (URL) {
+    let searched = document.getElementById('LiveSearch').value;
+
+    $.ajax({
+        type: "POST",
+        url: URL,
+        data: { value: searched},
+        success: function (result) {
+            $('#main').html( result) 
+           console.log('Success Search')
+
+        }
+    })
+    
+}
